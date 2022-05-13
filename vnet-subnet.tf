66f4412fb3582a27b11b3af57a6f60c4a67f12c8
Aaron <aortiz122442@gmail.com>
2022-05-13 22:18:55 +0000

    add

--- vnet-subnet.tf
+++ vnet-subnet.tf
@@ -8,6 +8,8 @@ resource "azurerm_virtual_network" "myterraformnetwork" {
 resource azurerm_subnet" "myterraformsubnet" {
     name = var.subnetname
     address_prefixes = ["10.0.2.0/24"]
-    resource_group_name = azurerm_resource_group.myTerraformGroup.name\n  
-    virtual_network_name = azurerm_virtual_network.myterraformnetwork
-}
+    resource_group_name = azurerm_resource_group.myTerraformGroup.name
+
+    virtual_network_name = azurerm_virtual_network.myterraformnetwork.name
+    
+    }
